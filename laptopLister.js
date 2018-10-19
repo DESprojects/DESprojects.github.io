172f207bbb501ecda783fb81c24ba89a47d6eaab
DESprojects <myherokelly@gmail.com>
2018-10-19 14:49:05 -0500

    Add laptopLister.js
    laptopLister bookmarklet

---
+++ laptopLister.js
@@ -0,0 +1,48 @@
+const title = document.querySelector('#app_bundle_product_form_title');
+const shortTitle = document.querySelector('#')
+const disclosure = document.querySelector('#app_bundle_product_form_carrier2')
+let addToEbay = document.querySelector('#app_bundle_product_form_addToEbay');
+const width = document.querySelector('#app_bundle_product_form_width');
+const height = document.querySelector('app_bundle_product_form_height');
+const depth = document.querySelector('app_bundle_product_form_height');
+
+setTimeout(main(), 500);
+
+//Main =============================================================================================
+function main(){
+    removeFromTitle();
+    addItemToEbay();
+    setDisclosure();
+    shipping();
+}
+//On load run functions
+//Functions ========================================================================================
+
+//Remove commas from Title
+function removeFromTitle(){
+    title.value = title.value.replace(/,/g, '');
+}
+//Change "Add to eBay" Value
+function addItemToEbay(){
+    addToEbay.value = '1';
+}
+
+//Set shipping disclosure
+function setDisclosure(){
+    disclosure.checked = true;
+}
+
+//Change shipping dimensions
+function shipping(){
+    width.value = '18';
+    height.value = '14';
+    depth.value = '8';
+    weight.value = '9';
+}
+
+//Future features ===================================================================================
+
+//Remove commas from Short Title
+function removeFromShortTitle(){
+
+}
